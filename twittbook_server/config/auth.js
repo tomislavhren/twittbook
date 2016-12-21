f9627d5588fdfccbd3974aff0c4799a0390bcdd7
Tomislav Hren <tomislav.hren@gmail.com>
2016-12-21 16:45:56 +0100

    asd

---
+++ twittbook_server/config/auth.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    'facebookAuth' : {
+            'clientID'      : '1145854572200935',
+            'clientSecret'  : 'd68137cadaf6a6d35717257fe0fea7ea',
+            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+}
---
+++ twittbook_server/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://nmilicic:rasusLab12@waffle.modulusmongo.net:27017/y4Sovere'
+
+};
