945d3ed85d0307d258f2124c6f8067b3fbf5f66d
Tomislav Hren <tomislav.hren@gmail.com>
2016-12-21 16:46:50 +0100

    asda

--- twittbok_server/config/auth.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    'facebookAuth' : {
-            'clientID'      : '1145854572200935',
-            'clientSecret'  : 'd68137cadaf6a6d35717257fe0fea7ea',
-            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
-    }
-}
--- twittbok_server/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://nmilicic:rasusLab12@waffle.modulusmongo.net:27017/y4Sovere'
-
-};
